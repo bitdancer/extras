204ccc7d30196b6216e9be5f0193d5cf5ad05273
R. David Murray <rdmurray@bitdance.com>
2017-03-22 01:45:01 -0400

    Import parameterize, add tests.
    This imports the parameterize code from one of the existing projects
    using it, renames it parameterizable_tests, and adds tests for the
    existing API, called here the legacy API.
    
    I started updating the module API before I realized I should check in
    the existing code and tests, so just ignore the partial module
    docstring.

---
+++ parameterizable_tests/test_parameterizable_tests.py
@@ -0,0 +1,97 @@
+import unittest
+
+from parameterizable_tests import parameterizable, parameterize
+
+
+class TestParaterizableTests(unittest.TestCase):
+
+    def xest_normal_tests_run(self):
+        res = []
+        @parameterizable
+        class Test(unittest.TestCase):
+            def test_normal_tests_run(self):
+                res.append(1)
+            @parameterize()
+            def test_foo(self):
+                raise Exception("This should not be run in this test")
+        Test(method='test_normal_tests_run').run()
+        self.assertEqual([1], res)
+
+
+class TestLegacyAPI(unittest.TestCase):
+
+    def test_normal_tests_run(self):
+        res = []
+        @parameterize
+        class Test(unittest.TestCase):
+            def test_normal_tests_run(self):
+                res.append(1)
+            foo_params = [1, 2]
+            def foo_as_bar(self, arg):
+                raise Exception("This should not be run in this test")
+        Test(methodName='test_normal_tests_run').run()
+        self.assertEqual([1], res)
+
+    def test_test_method_must_exist(self):
+        with self.assertRaisesRegex(ValueError, "(?i)No test"):
+            @parameterize
+            class Test(unittest.TestCase):
+                foo_params = [1, 2]
+                def test_with_wrong_name(self, arg):
+                    pass
+
+    def test_test_data_must_exist(self):
+        with self.assertRaisesRegex(ValueError, "(?i)No params"):
+            @parameterize
+            class Test(unittest.TestCase):
+                bad_params_name = [1, 2]
+                def foo_as_bar(self, arg):
+                    pass
+
+    def test_multiple_test_names_generated_automatically(self):
+        res = []
+        @parameterize
+        class Test(unittest.TestCase):
+            foo_params = [1, 2]
+            def foo_as_bar(self, arg):
+                res.append(arg)
+        Test(methodName='test_bar_1').run()
+        self.assertEqual([1], res)
+        Test(methodName='test_bar_2').run()
+        self.assertEqual([1, 2], res)
+
+    def test_names_generated_via_str(self):
+        res = []
+        @parameterize
+        class Test(unittest.TestCase):
+            foo_params = [0x30, 13.70]
+            def foo_as_bar(self, arg):
+                res.append(arg)
+        Test(methodName='test_bar_48').run()
+        self.assertEqual([48], res)
+        Test(methodName='test_bar_13.7').run()
+        self.assertEqual([48, 13.7], res)
+
+    def test_multipart_test_names_generated_automatically(self):
+        res = []
+        @parameterize
+        class Test(unittest.TestCase):
+            foo_params = [(1, 7), (2, 8)]
+            def foo_as_bar(self, arg1, arg2):
+                res.append((arg1, arg2))
+        Test(methodName='test_bar_1_7').run()
+        self.assertEqual([(1, 7)], res)
+        Test(methodName='test_bar_2_8').run()
+        self.assertEqual([(1, 7), (2, 8)], res)
+
+    def test_multiple_test_names_generated_from_dict_keys(self):
+        res = []
+        @parameterize
+        class Test(unittest.TestCase):
+            foo_params = dict(a=(1,), b=(2,))
+            def foo_as_bar(self, arg):
+                res.append(arg)
+        Test(methodName='test_bar_a').run()
+        self.assertEqual([1], res)
+        Test(methodName='test_bar_b').run()
+        self.assertEqual([1, 2], res)
